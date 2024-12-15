01d6db32e01c6addcd5d8e4a5bd4bc4b67a6c1ac
Rasmus <rasmustofthermansen@gmail.com>
2024-12-15 18:07:56 +0100

    Change entrypoint

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,15 +1,12 @@
 import { RemixBrowser } from "@remix-run/react";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
 
 startTransition(() => {
   hydrateRoot(
     document,
     <StrictMode>
-      <HashRouter basename="/freakyvillescammers/">
-        <RemixBrowser />
-      </HashRouter>
+      <RemixBrowser />
     </StrictMode>
   );
 });
