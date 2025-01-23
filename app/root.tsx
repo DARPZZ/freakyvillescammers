43a1db9a9d4eb3b1b6c7b192df622ee3eaad0629
Rasmus <rasmustofthermansen@gmail.com>
2025-01-23 15:58:51 +0100

    Tag test

--- app/root.tsx
+++ app/root.tsx
@@ -4,58 +4,33 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-  useLocation,
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 
 import styles from "./tailwind.css?url";
 import Navbar from "./Controllers/Navbar";
-import * as gtag from "~/util/gtags.client";
-import { useEffect } from "react";
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
-const gaTrackingId = "G-7L37WT09HW";
-
 export default function App() {
-  const location = useLocation();
-
-  useEffect(() => {
-    if (gaTrackingId?.length) {
-      gtag.pageview(location.pathname, gaTrackingId);
-    }
-  }, [location, gaTrackingId]);
-
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
+      
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-VPVC945BG2"></script>
+        <script dangerouslySetInnerHTML={{__html:`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-VPVC945BG2');`}}>
+        </script>
+        
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
       <body>
-        <>
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}
-          />
-          <script
-            async
-            id="gtag-init"
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-
-                gtag('config', '${gaTrackingId}', {
-                  page_path: window.location.pathname,
-                });
-              `,
-            }}
-          />
-        </>
         <div className="flex w-full h-dvh">
           <div className="w-2/4 md:w-1/4 flex justify-center items-center bg-blue-900 ">
             <Navbar />
