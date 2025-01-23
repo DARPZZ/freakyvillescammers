196c1681ffd80058f00a6db542822fff25f58815
Rasmus <rasmustofthermansen@gmail.com>
2025-01-23 15:43:15 +0100

    Gtags.client

--- app/root.tsx
+++ app/root.tsx
@@ -1,43 +1,65 @@
+import { json } from "@remix-run/node";
 import {
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
+  useLocation,
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 
 import styles from "./tailwind.css?url";
 import Navbar from "./Controllers/Navbar";
-
+import * as gtag from "~/util/gtags.client";
+import { useEffect } from "react";
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
+export const loader = async () => {
+  return json({ gaTrackingId: "G-7L37WT09HW"});
+};
+
 export default function App() {
+  const location = useLocation();
+  const { gaTrackingId } = useLoaderData<typeof loader>();
+  useEffect(() => {
+    if (gaTrackingId?.length) {
+      gtag.pageview(location.pathname, gaTrackingId);
+    }
+  }, [location, gaTrackingId]);
+
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
+
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-7L37WT09HW"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-7L37WT09HW', {
-        page_path: window.location.pathname,
-        });
-    `,
-          }}
-        />
       </head>
       <body>
+        <>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}
+          />
+          <script
+            async
+            id="gtag-init"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', '${gaTrackingId}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
+        </>
         <div className="flex w-full h-dvh">
           <div className="w-2/4 md:w-1/4 flex justify-center items-center bg-blue-900 ">
             <Navbar />
